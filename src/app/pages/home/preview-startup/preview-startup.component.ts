2108bceb4ba629d86723b69cea976b7812157ca3
abd <alsaadabood8@gmail.com>
2023-01-12 05:22:58 -0800

    push

--- src/app/pages/home/preview-startup/preview-startup.component.ts
+++ src/app/pages/home/preview-startup/preview-startup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Startup } from 'src/app/core/interfaces/startups.interface';
+import { StartupsService } from 'src/app/core/services/startup/startups.service';
 
 @Component({
   selector: 'app-preview-startup',
@@ -6,8 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preview-startup.component.css']
 })
 export class PreviewStartupComponent implements OnInit {
-  constructor(){}
+  arrayStartup:any ;
+  constructor(
+    private _startupService: StartupsService
+  ){}
+
   ngOnInit(): void {
+        this.getAllData();
+  }
+
+  getAllData() {
+    this._startupService.getAll().subscribe((result) => {
+      if (result) {
+        this.arrayStartup = result;
+        console.log(result);
+      }
+    });
   }
 
 }
